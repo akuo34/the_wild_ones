af5645ca0c9ebc16719ce31312bb7986e82436dd
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-11-10 12:15:43 -0800

    set up about page, rm jquery

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,40 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 export default function About() {
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = () => {
+    Axios
+      .get('/api/about')
+      .then(response => {
+        setImages(response.data);
+      })
+      .catch(err => console.error(err));
+  }
+
   return (
     <div>
       <Head>
         <title>About the Artist | The Wild Ones</title>
         <link rel="icon" href="/white_logo.jpg" />
       </Head>
+      <div className="buffer"></div>
+      <div className="container-gallery-page">
+        <h2 className="subheader-client">about the artist</h2>
+        {images.length ?
+          <div className="container-image-about">
+            <img className="image-about" src={images[0].portraitFireBaseUrl} alt="about-image"></img>
+            <p className="container-bio">{images[0].bio}</p>
+          </div> : null
+        }
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Slider from 'react-slick';
-// import $ from 'jquery';
 
 export default function Home(props) {
 
@@ -11,10 +10,6 @@ export default function Home(props) {
   useEffect(() => {
     getImages();
   }, []);
-
-  // $('body').on('contextmenu', 'img', (e) => {
-  //   return false;
-  // })
 
   const getImages = () => {
     Axios
@@ -41,42 +36,40 @@ export default function Home(props) {
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       </Head>
-      <div>
-        <div className="buffer"></div>
-        <div className="container-gallery-page">
-          <h2 className="subheader-client">art by candy kuo</h2>
-          {
-            images.length ?
-              <Slider className="slider" {...settings}>
-                {images.map((image, key) => {
-                  return (
-                    <div key={key} className="container-image-gallery">
-                      <img
-                        className="image-gallery"
-                        onClick={props.modalHandler}
-                        data-url={image.fireBaseUrl}
-                        src={image.fireBaseUrl}
-                        alt="gallery-image"></img>
-                    </div>
-                  )
-                })}
-              </Slider> : null
-          }
-          <div className="container-grid">
-            {images.map((image, key) => {
-              return (
-                <div key={key} className="container-image-grid">
-                  <img
-                    className="image-grid"
-                    onClick={props.modalHandler}
-                    data-url={image.fireBaseUrl}
-                    loading="lazy"
-                    src={image.fireBaseUrl}
-                    alt="gallery-image"></img>
-                </div>
-              )
-            })}
-          </div>
+      <div className="buffer"></div>
+      <div className="container-gallery-page">
+        <h2 className="subheader-client">art by candy kuo</h2>
+        {
+          images.length ?
+            <Slider className="slider" {...settings}>
+              {images.map((image, key) => {
+                return (
+                  <div key={key} className="container-image-gallery">
+                    <img
+                      className="image-gallery"
+                      onClick={props.modalHandler}
+                      data-url={image.fireBaseUrl}
+                      src={image.fireBaseUrl}
+                      alt="gallery-image"></img>
+                  </div>
+                )
+              })}
+            </Slider> : null
+        }
+        <div className="container-grid">
+          {images.map((image, key) => {
+            return (
+              <div key={key} className="container-image-grid">
+                <img
+                  className="image-grid"
+                  onClick={props.modalHandler}
+                  data-url={image.fireBaseUrl}
+                  loading="lazy"
+                  src={image.fireBaseUrl}
+                  alt="gallery-image"></img>
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
