6cd4c1d5013ea819abe9b0e548f88df4c58a5836
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-12-17 14:59:21 -0800

    chnage thumbnail size to 300 x 350

--- pages/admin/gallery.js
+++ pages/admin/gallery.js
@@ -42,7 +42,7 @@ export default function GalleryManager(props) {
   }
 
   const resizeFile = (file) => new Promise(resolve => {
-    Resizer.imageFileResizer(file, 250, 300, 'JPEG', 100, 0,
+    Resizer.imageFileResizer(file, 300, 350, 'JPEG', 100, 0,
     // Resizer.imageFileResizer(file, 250, 150, 'JPEG', 100, 0,
       uri => {
         resolve(uri);
